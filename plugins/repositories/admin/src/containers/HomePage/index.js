b9a8d4a1223129dbd9ae720eafff2aea0bd53cf0
Ian Libânio <huetroll1@gmail.com>
2021-07-21 23:07:37 -0300

    feat: create repositories plugin

---
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, memo } from "react";
+
+import axios from "axios";
+import styled from "styled-components";
+
+import { Table } from "@buffetjs/core";
+import { Header } from "@buffetjs/custom";
+
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1.5rem;
+  }
+`;
+
+const headers = [
+  {
+    name: "Name",
+    value: "name",
+  },
+  {
+    name: "Description",
+    value: "description",
+  },
+  {
+    name: "Url",
+    value: "html_url",
+  },
+];
+
+// https://api.github.com/users/react-avancado/repos
+const HomePage = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/ianlibanio/repos")
+      .then((res) => {
+        res.data.map((data) => {
+          if (data.name.startsWith('wongames')) setRows(oldRows => [oldRows, data])
+        })
+      })
+      .catch(e => strapi.notification.error(`${e}`));
+  }, []);
+
+  return (
+    <Wrapper>
+      <Header
+        title={{ label: "Repositories" }}
+        content="A list of Won Games repositories."
+      />
+      <Table
+        headers={headers}
+        rows={rows}
+        onClickRow={(e, data) => {
+          window.open(data.html_url);
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+export default memo(HomePage);
